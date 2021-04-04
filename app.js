4d36a8877ba66ac2911683c63ef4661a1238c1f3
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-04-04 14:20:06 +0300

    my lab05

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+var carType = prompt('Plese enter care type');
+while(carType !== 'toyota' && carType !== 'kia'){
+    carType = prompt('Plese choose toyota or kia');
+}
+var userInput = prompt('Plese enter number of the car!');
+
+for (var i = 0; i < userInput; i++){
+    if(carType == 'toyota') {
+        document.write('<img src="https://freevectorlogo.net/wp-content/uploads/2011/04/toyota-logo-vector-400x400.png" />');
+    }else {
+        document.write('<img src="http://topsat-eg.com/images/p12609f152b4f0d7fadb39d55b10adf51.png" />');
+    }
+}
+
+
 alert("JavaScript page Test");
 var userName = "Nedal";
 document.write(userName);
